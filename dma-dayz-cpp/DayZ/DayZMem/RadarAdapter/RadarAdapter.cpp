8d91e19a1729cf5f45d8394bb5174129f7d98d68
Arkadiusz Dudek <adudek013@gmail.com>
2023-07-30 16:26:58 -0500

    Text in radar

--- dma-dayz-cpp/DayZ/DayZMem/RadarAdapter/RadarAdapter.cpp
+++ dma-dayz-cpp/DayZ/DayZMem/RadarAdapter/RadarAdapter.cpp
@@ -9,9 +9,22 @@ void DayZ::RadarAdapter::DrawOverlay(DMARender::IGameMap* curMap, const DMARende
 {
 	auto drawList = ImGui::GetBackgroundDrawList();
 	auto nearEnts = this->memUpdater->getNearEntityTable()->resolvedEntities;
+	ImGui::PushFont(radarFont);
 	for (auto ent : nearEnts) {
 		ImVec2 screenPos = WorldToRadar(curMap, mTransform, ent->FutureVisualStatePtr->position);
 		//drawList->AddCircleFilled(screenPos, 10, IM_COL32(255, 0, 0, 255), 10);
-		drawBlip(screenPos, 6, IM_COL32(255, 0, 0, 255));
+
+		drawBlip(screenPos, 6, IM_COL32(255, 0, 0, 255), 16, 1, {"Test", "One", "Two"});
+
 	}
+	ImGui::PopFont();
+}
+
+void DayZ::RadarAdapter::createFonts()
+{
+		ImFontConfig config;
+		config.OversampleH = 3;
+		config.OversampleV = 1;
+		config.GlyphExtraSpacing.x = 1.0f;
+		radarFont = ImGui::GetIO().Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Calibri.ttf", 32, &config);
 }
--- dma-dayz-cpp/DayZ/DayZMem/RadarAdapter/RadarAdapter.h
+++ dma-dayz-cpp/DayZ/DayZMem/RadarAdapter/RadarAdapter.h
@@ -5,12 +5,14 @@ namespace DayZ {
 	class RadarAdapter : public DMARender::IRadar {
 
 		DayZ::MemoryUpdater* memUpdater;
+		ImFont* radarFont;
 	public:
 		RadarAdapter(DayZ::MemoryUpdater* memUpdater);
 
 
 		// Inherited via IRadar
 		virtual void DrawOverlay(DMARender::IGameMap* curMap, const DMARender::MapTransform& mTransform) override;
+		virtual void createFonts() override;
 
 	};
 }
