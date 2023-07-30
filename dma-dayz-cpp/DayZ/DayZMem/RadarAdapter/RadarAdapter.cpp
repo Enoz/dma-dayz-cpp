33cad52a72547eab6ddca97877af82bf3d6b1810
Arkadiusz Dudek <adudek013@gmail.com>
2023-07-30 15:31:42 -0500

    WorldToRadar sig

--- dma-dayz-cpp/DayZ/DayZMem/RadarAdapter/RadarAdapter.cpp
+++ dma-dayz-cpp/DayZ/DayZMem/RadarAdapter/RadarAdapter.cpp
@@ -9,13 +9,8 @@ void DayZ::RadarAdapter::DrawOverlay(DMARender::IGameMap* curMap, const DMARende
 {
 	auto drawList = ImGui::GetBackgroundDrawList();
 	auto nearEnts = this->memUpdater->getNearEntityTable()->resolvedEntities;
-	ImGui::Begin("Debug Radar");
 	for (auto ent : nearEnts) {
-		ImVec2 screenPos;
-		if (WorldToRadar(curMap, mTransform, ent->FutureVisualStatePtr->position, screenPos)) {
-			drawList->AddCircleFilled(screenPos, 10, IM_COL32(255, 0, 0, 255), 10);
-			ImGui::Text("GamePos(%.1f, %.1f, %.1f), ScreenPos: (%.1f, %.1f)", ent->FutureVisualStatePtr->position.x, ent->FutureVisualStatePtr->position.y, ent->FutureVisualStatePtr->position.z, screenPos.x, screenPos.y);
-		}
+		ImVec2 screenPos = WorldToRadar(curMap, mTransform, ent->FutureVisualStatePtr->position);
+		drawList->AddCircleFilled(screenPos, 10, IM_COL32(255, 0, 0, 255), 10);
 	}
-	ImGui::End();
 }
